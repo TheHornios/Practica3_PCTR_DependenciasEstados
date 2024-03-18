5308d64400f5ee038fdbb540fbc015ec7d385736
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2024-03-19 00:25:22 +0100

    Comentar la clase Sitemalanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,15 +1,24 @@
 package src.p03.c01;
 
+/**
+ * SistemaLanzador.java
+ * Este archivo contiene la clase principal que inicia la simulación del parque.
+ * 
+ * @author Rordrigo Pascual Arnaiz
+ */
 public class SistemaLanzador {
 	public static void main(String[] args) {
 		
-		IParque parque = new Parque(); // TODO
-		char letra_puerta = 'A';
-		
+		IParque parque = new Parque(); // Creación de un objeto Parque para simular el parque
+
+		char letra_puerta = 'A'; // Definición de la letra inicial para las puertas
+
 		System.out.println("¡Parque abierto!");
 		
-		for (int i = 0; i < 5; i++) { // Integer.parseInt(args[0])
+		// Ciclo para crear hilos de entrada y salida para cada puerta del parque
+		for (int i = 0; i < 5; i++) { 
 			
+            // Generación del identificador de la puerta
 			String puerta = ""+((char) (letra_puerta++));
 			
 			// Creación de hilos de entrada
