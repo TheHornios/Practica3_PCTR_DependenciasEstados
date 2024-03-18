a8dbda224a16a53d8896e9cb96d30ca7690b06e6
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2024-03-18 20:44:06 +0100

    Añadir la clase actividad salida y la interfaz de Iparque

---
+++ src/p03/c01/SistemaLanzador.java
@@ -0,0 +1,27 @@
+package src.p03.c01;
+
+public class SistemaLanzador {
+	public static void main(String[] args) {
+		
+		IParque parque = new Parque(); // TODO
+		char letra_puerta = 'A';
+		
+		System.out.println("¡Parque abierto!");
+		
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+			
+			String puerta = ""+((char) (letra_puerta++));
+			
+			// Creación de hilos de entrada
+			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+			new Thread (entradas).start();
+			
+			// Creacion de hilos de salida 
+
+			ActividadSalidaPuerta salida = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salida).start();
+			
+			
+		}
+	}	
+}
