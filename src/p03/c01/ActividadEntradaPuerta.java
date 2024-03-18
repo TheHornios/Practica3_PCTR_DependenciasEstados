5ef5ab6e14f6d5ce84dbd3f5f0dc134280ef3b7e
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2024-03-19 00:28:34 +0100

    Añadir comentatrios a la clase salida y entrada

--- src/p03/c01/ActividadEntradaPuerta.java
+++ src/p03/c01/ActividadEntradaPuerta.java
@@ -5,22 +5,41 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * La clase ActividadEntradaPuerta implementa la interfaz Runnable
+ * para simular la actividad de entrada de visitantes a través de una puerta del parque.
+ * 
+ * @author Rordrigo Pascual Arnaiz
+ */
 public class ActividadEntradaPuerta implements Runnable{
-
+	
+    	// Número total de entradas que se realizarán en la puerta
 		private static final int NUMENTRADAS = 20;
-		private String puerta;
-		private IParque parque;
+		private String puerta;  // Identificador de la puerta
+		private IParque parque; // Instancia del parque sobre el que se realizarán las entradas
 
+		/**
+	     * Constructor de la clase ActividadEntradaPuerta.
+	     * @param puerta Identificador de la puerta por la que ingresarán los visitantes.
+	     * @param parque Instancia del parque sobre el que se realizarán las entradas.
+	     */
 		public ActividadEntradaPuerta(String puerta, IParque parque) {
 			this.puerta = puerta;
 			this.parque = parque;
 		}
 
+		/**
+	     * Método run que se ejecutará cuando se inicie el hilo.
+	     * Simula las entradas de visitantes al parque a través de la puerta especificada.
+	     */
 		@Override
 		public void run() {
+	        // Ciclo para simular las entradas a través de la puerta
 			for (int i = 0; i < NUMENTRADAS; i ++) {
 				try {
+	                // Llamada al método para registrar la entrada al parque
 					parque.entrarAlParque(puerta);
+	                // Simulación de un tiempo de espera aleatorio antes de la siguiente entrada
 					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
 				} catch (InterruptedException e) {
 					Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,26 +1,47 @@
 package src.p03.c01;
 
 import java.util.Random;
+
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * La clase ActividadSalidaPuerta implementa la interfaz Runnable
+ * para simular la actividad de salida de visitantes a través de una puerta del parque.
+ * 
+ * @author Rordrigo Pascual Arnaiz
+ */
 public class ActividadSalidaPuerta implements Runnable{
 
+    // Número total de salidas que se realizarán en la puerta
 	private static final int NUMSALIDAS= 20;
+    // Identificador de la puerta
 	private String puerta;
+    // Instancia del parque sobre el que se realizarán las entradas
 	private IParque parque;
 
+	 /**
+     * Constructor de la clase ActividadSalidaPuerta.
+     * @param puerta Identificador de la puerta por la que ingresarán los visitantes.
+     * @param parque Instancia del parque sobre el que se realizarán las salidas.
+     */
 	public ActividadSalidaPuerta(String puerta, IParque parque) {
 		this.puerta = puerta;
 		this.parque = parque;
 	}
 
+	/**
+     * Método run que se ejecutará cuando se inicie el hilo.
+     * Simula las salidas de visitantes al parque a través de la puerta especificada.
+     */
 	@Override
 	public void run() {
 		for (int i = 0; i < NUMSALIDAS; i ++) {
 			try {
+                // Llamada al método para registrar la salida al parque
 				this.parque.salidaDelParque(puerta);
+                // Simulación de un tiempo de espera aleatorio antes de la siguiente salida
 				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
 			} catch (InterruptedException e) {
 				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
